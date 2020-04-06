8c2a8edcf0653d89231f4fa7df44120697c73a3a
y-nakao3875 <y-nakao3875@lk.lbm.co.jp>
2020-04-06 14:31:51 +0900

    Updates

---
+++ precache-manifest.e9a4b2d20c4af2d1b5d4dad21fbb00a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b81de951b186bdb2c188a4758d63322",
+    "url": "/React-test-COVID19/index.html"
+  },
+  {
+    "revision": "99dfc0d236e1f462831e",
+    "url": "/React-test-COVID19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6ee5439ac4bf30c8a4a3",
+    "url": "/React-test-COVID19/static/js/2.73acb3ee.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/React-test-COVID19/static/js/2.73acb3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99dfc0d236e1f462831e",
+    "url": "/React-test-COVID19/static/js/main.5dfeb859.chunk.js"
+  },
+  {
+    "revision": "f4955233cb653d95023f",
+    "url": "/React-test-COVID19/static/js/runtime-main.911a2c9b.js"
+  }
+]);
